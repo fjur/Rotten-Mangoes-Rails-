06f7363db48f14db3094c67912e677c272f439ed
fjur <fjuristovski@gmail.com>
2016-03-28 23:57:18 +0000

    Added sessions and logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
    @user = User.new(user_params)
 
    if @user.save
-     redirect_to movies_path
+    session[:user_id] = @user.id
+    redirect_to movies_path
    else
      render :new
    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   get 'users/new'
 
   get 'users/create'
@@ -14,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
   # root 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
