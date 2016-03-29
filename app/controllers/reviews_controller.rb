cb23e3af0dcc067af744f374433021b7be8d0fef
fjur <fjuristovski@gmail.com>
2016-03-29 03:38:23 +0000

    refactor reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   
+  before_filter :load_movie
+
   def new
-    @movie = Movie.find(params[:id])
     @review = @movie.reviews.build
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(review_params)
     @review.user_id = current_user.id
 
@@ -16,8 +16,12 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
-  
+
   protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
