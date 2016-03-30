7655fd5f1505fcccf20d3dd374ec4d809a1f2d88
fjur <fjuristovski@gmail.com>
2016-03-30 00:35:51 +0000

    can edit users now

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,6 +21,21 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+
+  end
+
   def index
     @users = User.all.page(params[:page]).per(2)
   end
