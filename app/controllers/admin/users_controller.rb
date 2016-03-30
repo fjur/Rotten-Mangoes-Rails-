63d64201d7989a389990126462e972123b67e235
fjur <fjuristovski@gmail.com>
2016-03-30 18:27:13 +0000

    added email to deleted users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -43,6 +43,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    UserMailer.delete_user_email(@user)
     redirect_to admin_users_path
   end
 
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def delete_user_email(user)
+    @user = user
+    @url = 'www.whocares.com'
+    mail(to: @user.email, subject: 'Your account got deleted')
+  end
+end
